ecb2ccec8dd7729a755069f9e0a23a7d07dcb946
choudharyjayant <104073223+choudharyjayant@users.noreply.github.com>
2022-05-27 13:56:47 +0530

    Create provider.tf

---
+++ Terraform/provider.tf
@@ -0,0 +1,19 @@
+ provider "aws" {
+   region = "eu-west-3"
+ }
+
+terraform {
+  required_version = ">= 1.1.2"
+  backend "s3" {}
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = ">= 3.70.0, >= 4.8.0" 
+      region = "ap-south-1"
+    }
+    tls = {
+      source = "hashicorp/tls"
+      version = "~> 3.1.0"
+    }
+  }
+}
