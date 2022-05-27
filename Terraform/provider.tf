da698d88b34229983d8fa6dd9f3db1932ae14838
choudharyjayant <104073223+choudharyjayant@users.noreply.github.com>
2022-05-27 16:00:19 +0530

    Update provider.tf

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -8,7 +8,10 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = ">= 3.70.0, >= 4.8.0"   
+      version = ">= 3.70.0, >= 4.8.0" 
+      region = "eu-west-3"
+     bucket = "bucket-jenkinspipeline"
+     key    = "magicbox-platform/dev/terraform.tfstate"
     }
     tls = {
       source = "hashicorp/tls"
