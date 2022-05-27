4e8af081f0ce6f25a40646ac0d10ebf9a76ce120
choudharyjayant <104073223+choudharyjayant@users.noreply.github.com>
2022-05-27 15:32:58 +0530

    Update provider.tf

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -8,8 +8,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = ">= 3.70.0, >= 4.8.0" 
-      
+      version = ">= 3.70.0, >= 4.8.0"   
     }
     tls = {
       source = "hashicorp/tls"
