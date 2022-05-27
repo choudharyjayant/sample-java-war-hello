1756cb41ce52d3f22cb39c91a4a8055338b4449c
choudharyjayant <104073223+choudharyjayant@users.noreply.github.com>
2022-05-27 10:22:44 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "eu-west-3"
-  access_key = "AKIAXE43QNLYDTXYM7OO"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
