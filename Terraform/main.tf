c91843ed8663ea14073f737e0b9bddb948434159
choudharyjayant <104073223+choudharyjayant@users.noreply.github.com>
2022-05-27 13:54:26 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,5 +1,66 @@
-provider "aws" {
-  region     = "eu-west-3"
-  access_key = ""
-  secret_key = ""
+resource "aws_elastic_beanstalk_application" "tftest" {
+  name        = var.elasticapp
+}
+
+resource "aws_elastic_beanstalk_environment" "terraform-Test" {
+  name                = var.beanstalkappenv
+  application         = aws_elastic_beanstalk_application.tftest.name
+  solution_stack_name = var.solution_stack_name
+  tier                = var.tier
+setting {
+        namespace = "aws:autoscaling:launchconfiguration"
+        name      = "IamInstanceProfile"
+        value     = "aws-elasticbeanstalk-ec2-role"
+      }
+setting {
+    namespace = "aws:ec2:vpc"
+    name      = "VPCId"
+    value     = var.vpc_id
+  }
+setting {
+    namespace = "aws:ec2:vpc"
+    name      = "AssociatePublicIpAddress"
+    value     =  "True"
+  }
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "Subnets"
+    value     = join(",", var.public_subnets)
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:environment:process:default"
+    name      = "MatcherHTTPCode"
+    value     = "200"
+  }
+
+  setting {
+    namespace = "aws:elasticbeanstalk:environment"
+    name      = "LoadBalancerType"
+    value     = "application"
+  }
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "InstanceType"
+    value     = "t2.micro"
+  }
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "ELBScheme"
+    value     = "internet facing"
+  }
+  setting {
+    namespace = "aws:autoscaling:asg"
+    name      = "MinSize"
+    value     = 1
+  }
+  setting {
+    namespace = "aws:autoscaling:asg"
+    name      = "MaxSize"
+    value     = 2
+}
+  setting {
+    namespace = "aws:elasticbeanstalk:healthreporting:system"
+    name      = "SystemType"
+    value     = "enhanced"
+  }
 }
